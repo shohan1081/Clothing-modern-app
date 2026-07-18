46f07bfea370188afd4febd28fc3cdf6cf4ab8bf
Shohanul Islam <shohanulislam19892@gmail.com>
2026-07-18 21:26:49 +0600

    config: route api/affiliate requests to affiliate app urls

--- Config/urls.py
+++ Config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/legal/', include('legal_pages.urls')),
+    path('api/affiliate/', include('affiliate.urls')),
 ]
